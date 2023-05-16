e42b4d426a26a7b2ef31252db4dd0de3f6edf5ec
victor <victor@victors-MacBook-Air.local>
2023-05-15 22:28:02 -0400

    i added a new image to the workflowdata

--- src/components/WorkFlowData.js
+++ src/components/WorkFlowData.js
@@ -1,10 +1,17 @@
 import pro1 from "../pics/SlickChef.png";
+import prop2 from "../pics/VASC.png";
 const projectFlowData = [
   {
     imgsrc: pro1,
     title: "SlickChef",
     text: "SlickChef is designed to help you generate recipes using the ingredients you have available and our AI-backed web app. You can also modify and save these recipes in your account to create your own personalized cookbook. In addition, our web app provides nutritional information about your recipe our health-conscious clients. Technologies used are html,css,javasript,django,python,postgreSQL,materialize.",
     view: "https://slickchef.herokuapp.com/",
+  },
+  {
+    imgsrc: prop2,
+    title: "VASC",
+    text: "VASC is an online e-commerce app that has items one can shop from. Items one can shop from varies from building items, school items, home items, electronics and gadgets. Technologies used incliude react,mongoose,mongoDB,node.js,html,css,javascript.",
+    view: "https://va-shopping-center.herokuapp.com/",
   },
 ];
 
